8db96a27d12b3a9e08f91f4d863fde3461daea06
chrisonntag <sonntagc@fim.uni-passau.de>
2017-01-06 20:46:11 +0100

    Add basic frontend
    Most static files are based on [puikinshs
    'Gentelella'](https://github.com/puikinsh/gentelella).

--- mmetering/views.py
+++ mmetering/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from mmetering.models import MeterData
 
-# Create your views here.
+
+
+def render_home(request):
+  """A view of all bands."""
+  data = MeterData.objects.all()
+  return render(request, 'mmetering/home.html', {'data': data})
