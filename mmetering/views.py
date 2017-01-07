be6e9572945636341acbc8683b1d0e7b9438ca28
chrisonntag <info@chrisonntag.com>
2017-01-07 02:15:54 +0100

    Setup first templates and fill them with data

--- mmetering/views.py
+++ mmetering/views.py
@@ -1,9 +1,33 @@
+import datetime
 from django.shortcuts import render
-from mmetering.models import MeterData
-
-
+from django.db.models import Max, Sum, Count, F, Avg, Q
+from mmetering.models import Flat, Meter, MeterData
 
 def render_home(request):
-  """A view of all bands."""
   data = MeterData.objects.all()
-  return render(request, 'mmetering/home.html', {'data': data})
+  import_ids = data.filter(meter__flat__mode__exact='IM').values('meter_id').annotate(max_value=Max('value')).order_by()
+
+  q_statement = Q()
+  for pair in import_ids:
+    q_statement |= (Q(meter_id__exact=pair['meter_id']) & Q(value=pair['max_value']))
+
+  current_month = datetime.datetime.now().month
+  current_year = datetime.datetime.now().year
+  last_month = datetime.datetime.now().month - 1 if datetime.datetime.now().month > 1 else 12
+  last_year = datetime.datetime.now().year - 1
+
+  return render(
+    request,
+    'mmetering/home.html',
+    {
+      'verbraucher': Flat.objects.filter(mode='IM'),
+      'lieferaten': Flat.objects.filter(mode='EX'),
+      'total': sum(data.filter(q_statement).values_list('value', flat=True).distinct()),
+      'flats': Flat.objects.all(),
+      'meter': Meter.objects.all(),
+      'data': data,
+      'avg_current': data.filter(saved_time__month=current_month, saved_time__year=current_year).aggregate(Avg('value')),
+      'avg_last': data.filter(saved_time__month=last_month, saved_time__year=last_year).aggregate(Avg('value')),
+      'avg_div': {'value__avg': 5.5}
+    }
+  )
