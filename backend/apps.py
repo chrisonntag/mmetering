4f28891505cd2bf334b3a77be2ce545553b56de5
chrisonntag <info@chrisonntag.com>
2017-05-17 18:39:20 +0200

    Add signal on MeterData's post_save

--- backend/apps.py
+++ backend/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class BackendConfig(AppConfig):
     name = 'backend'
+    verbose_name = 'Backend Application'
+
+    def ready(self):
+        import backend.signals
