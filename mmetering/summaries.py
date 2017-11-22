90d6eebf1cb81bc30ae897f95e0870e50051ec87
chrisonntag <info@chrisonntag.com>
2017-11-22 23:53:29 +0100

    Handle IndexError in is_supply_over_threshold method in case theres no data

--- mmetering/summaries.py
+++ mmetering/summaries.py
@@ -153,8 +153,12 @@ class Overview:
         s_data = self.get_data_range(self.times['now-1'], datetime.today(), 'EX').order_by('-value_sum')[:2]
 
         if c_data and s_data:
-            consumption = c_data[0].get('value_sum') - c_data[1].get('value_sum')
-            supply = s_data[0].get('value_sum') - s_data[1].get('value_sum')
+            try:
+                consumption = c_data[0].get('value_sum') - c_data[1].get('value_sum')
+                supply = s_data[0].get('value_sum') - s_data[1].get('value_sum')
+            except IndexError:
+                logging.warning('Not enough data for checking if own supply is over a threshold.')
+                return False
 
             if supply >= consumption * threshold:
                 return True
