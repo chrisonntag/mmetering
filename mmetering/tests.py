b26e63fbc5a9fd5a2979f1d242823f5e81793ae6
chrisonntag <info@chrisonntag.com>
2017-11-26 21:11:32 +0100

    Complete tests for Overview class

--- mmetering/tests.py
+++ mmetering/tests.py
@@ -15,11 +15,11 @@ class SummariesDataTest(TestCase):
     def test_parse_date(self):
         start_of_the_day = datetime(2017, 11, 11, 0, 0, 0, 0)
         end_of_the_day = datetime(2017, 11, 11, 23, 59, 59, 0)
-        self.assertEquals(SummariesDataTest.data.parse_date('11.11.2017', False), start_of_the_day)
-        self.assertEquals(SummariesDataTest.data.parse_date('11.11.2017', True), end_of_the_day)
-        self.assertRaises(ValueError, SummariesDataTest.data.parse_date('11/11/2017', True))
-        self.assertRaises(ValueError, SummariesDataTest.data.parse_date('40.13.2017', True))
-        self.assertRaises(ValueError, SummariesDataTest.data.parse_date('11.12.017', True))
+        self.assertEquals(Overview.parse_date('11.11.2017', False), start_of_the_day)
+        self.assertEquals(Overview.parse_date('11.11.2017', True), end_of_the_day)
+        self.assertRaises(ValueError, Overview.parse_date('11/11/2017', True))
+        self.assertRaises(ValueError, Overview.parse_date('40.13.2017', True))
+        self.assertRaises(ValueError, Overview.parse_date('11.12.017', True))
 
     def test_get_data_range(self):
         start_date = datetime(2017, 2, 4, 8, 0)
@@ -32,18 +32,28 @@ class SummariesDataTest(TestCase):
 
         assert len(import_range) == 4
         assert len(export_range) == 4
-        self.assertAlmostEqual(import_range[3]['value_sum'], 17807, 2)
-        self.assertAlmostEqual(export_range[2]['value_sum'], 8, 2)
+        self.assertAlmostEqual(import_range[3]['value_sum'], 21902, 2)  # 08:45
+        self.assertAlmostEqual(export_range[2]['value_sum'], 13154860.4, 2)  # 08:30
         self.assertListEqual(list(import_range_empty), [])
 
     def test_get_total(self):
-        pass
+        end_date_import = datetime(2017, 2, 7, 0, 0, 0)
+        end_date_export = datetime(2017, 2, 4, 14, 45, 0)
+        asserted_values_import = [86.507, 91.699, 82.495, 41.283]
+        asserted_values_export = [15.658, 13163.4786]
+
+        self.assertListEqual(SummariesDataTest.data.get_total(end_date_import, 'IM'), asserted_values_import)
+        self.assertListEqual(SummariesDataTest.data.get_total(end_date_export, 'EX'), asserted_values_export)
 
     def test_get_total_consumption(self):
-        pass
+        end_date = datetime(2017, 2, 7, 0, 0, 0)
+
+        self.assertAlmostEqual(SummariesDataTest.data.get_total_consumption(end_date), 0.301984, 3)
 
     def test_get_day_consumption(self):
-        pass
+        end_date = datetime(2017, 2, 6, 0, 0, 0)
+
+        self.assertAlmostEqual(SummariesDataTest.data.get_day_consumption(end_date), 109.523, 3)
 
     def test_supply_over_threshold(self):
         with freeze_time('2017-02-04 12:42:34'):
